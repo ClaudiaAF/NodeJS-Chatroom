9191d977ec1a8ac930cf654a288c9060dc301ee6
Claudia <180181@virtualwindow.co.za>
2020-06-11 17:19:30 +0200

    plagerism form added and code formatted

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const http = require('http');
 const queryString = require('querystring');
 const server = http.createServer();
 const fs = require('fs')
-const template = require ('es6-template-strings');
+const template = require('es6-template-strings');
 const loggedInUsers = []
 var bodyString = '';
 
-var io = require('socket.io')(server) 
+var io = require('socket.io')(server)
 
 io.on('connection', (socket) => {
   console.log("a user has connected")
@@ -14,186 +14,174 @@ io.on('connection', (socket) => {
 
   socket.on('chatroom', (msg) => {
     console.log(`this was a message from chat room: ${msg}`)
-    io.emit('chatroom', msg) 
+    io.emit('chatroom', msg)
   })
 })
 
 
 var simpleRouter = (request) => {
-    var method = request.method;
-    var path = request.url;
-    var queryIndex = request.url.indexOf('?');
-    if (queryIndex > 0) {
-        path = request.url.slice(0, queryIndex)
+  var method = request.method;
+  var path = request.url;
+  var queryIndex = request.url.indexOf('?');
+  if (queryIndex > 0) {
+    path = request.url.slice(0, queryIndex)
+  }
+  var suppliedRoute = { method: method, path: path };
+  var routes = [
+    { method: 'GET', path: '/', handler: handleFormGet },
+    { method: 'POST', path: '/', handler: handleFormPost },
+    { method: 'GET', path: '/', handler: handleFormGet },
+    { method: 'POST', path: '/', handler: handleFormPost },
+    { method: 'GET', path: '/log-in', handler: handleLoginGet },
+    { method: 'POST', path: '/log-in', handler: handleLoginPost },
+    { method: 'GET', path: '/chatroom', handler: handleChatGet }
+  ];
+  for (let i = 0; i < routes.length; i++) {
+    var route = routes[i]
+    if (route.method === suppliedRoute.method && route.path === suppliedRoute.path) {
+      return route.handler;
     }
-    var suppliedRoute = {method: method, path: path};
-    var routes = [
-        {method: 'GET', path: '/', handler: handleFormGet},
-        {method: 'POST', path: '/', handler: handleFormPost},
-        {method: 'GET', path: '/', handler: handleFormGet},
-        {method: 'POST', path: '/', handler: handleFormPost},
-        {method: 'GET', path: '/log-in', handler: handleLoginGet},
-        {method: 'POST', path: '/log-in', handler: handleLoginPost},
-        {method: 'GET', path: '/chatroom', handler: handleChatGet}
-    ];
-    for (let i = 0; i < routes.length; i++) {
-        var route = routes[i]
-        if (route.method === suppliedRoute.method && route.path === suppliedRoute.path) {
-            return route.handler;
-        }
-    }
-    return null;
+  }
+  return null;
 }
 
 
-var handleFormGet = function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('template/form.html', 'utf8', function(err, data) {
-      if (err) { throw err; }
-      response.write(data);
-      response.end(bodyString);
-    });
-  };
-
-  var handleProfileGet = function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('template/profile.html', 'utf8', function(err, data) {
-      if (err) { throw err; }
-      response.write(data);
-      response.end(bodyString);
-    });
-  };
-
-  var handleLoginGet = function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('template/login.html', 'utf8', function(err, data) {
-      if (err) { throw err; }
-      response.write(data);
-      response.end(bodyString);
-    });
-  };
-
-  var handleChatGet = function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('template/dashboard.html', 'utf8', function(err, data) {
-      if (err) { throw err; }
-      response.write(data);
-      response.end(bodyString);
-    });
-  };
-
-  var handleFormPost = (request, response) => {
-    
-    response.writeHead(200, {"Content-Type": "text/html"});
-    var bodyString = '';
-    request.on('data', function (data) {
-    bodyString += data;
-
-    });
-
-    request.on('end', function () {
-    var post = queryString.parse(bodyString);
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('template/dashboard.html', 'utf8', (err, data) => {
-        if (err) { throw err }
-        var values ={
-            username: post['username'], 
-        }
-            ;
-
-        var compiled = template(data, values)
-        response.write(compiled);
-        response.end();
-         })
-        })
+var handleFormGet = function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile('template/form.html', 'utf8', function (err, data) {
+    if (err) { throw err; }
+    response.write(data);
+    response.end(bodyString);
+  });
 };
 
-var handleLoginPost = (request, response) => {
-    
-    response.writeHead(200, {"Content-Type": "text/html"});
-    var bodyString = '';
-    request.on('data', function (data) {
-    bodyString += data;
+var handleProfileGet = function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile('template/profile.html', 'utf8', function (err, data) {
+    if (err) { throw err; }
+    response.write(data);
+    response.end(bodyString);
+  });
+};
 
-    });
+var handleLoginGet = function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile('template/login.html', 'utf8', function (err, data) {
+    if (err) { throw err; }
+    response.write(data);
+    response.end(bodyString);
+  });
+};
 
-    request.on('end', function () {
-    var post = queryString.parse(bodyString);
-    loggedInUsers.push(post)
-    response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('template/dashboard.html', 'utf8', (err, data) => {
-        if (err) { throw err }
-        var values ={
-            username: post['username'],
-            loggedUsers:listLoginUsers(loggedInUsers)
-        }
-        var compiled = template(data, values)
-        response.write(compiled);
-        response.end();
-      });
-    
-    });
-}
+var handleChatGet = function (request, response) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile('template/dashboard.html', 'utf8', function (err, data) {
+    if (err) { throw err; }
+    response.write(data);
+    response.end(bodyString);
+  });
+};
 
-var handleNavigationPost = (request, response) => {
-    
-  response.writeHead(200, {"Content-Type": "text/html"});
+var handleFormPost = (request, response) => {
+
+  response.writeHead(200, { "Content-Type": "text/html" });
   var bodyString = '';
   request.on('data', function (data) {
-  bodyString += data;
+    bodyString += data;
 
   });
 
   request.on('end', function () {
-  var post = queryString.parse(bodyString);
-  loggedInUsers.push(post)
-  response.writeHead(200, {"Content-Type": "text/html"});
-  fs.readFile('template/chat2.html', 'utf8', (err, data) => {
+    var post = queryString.parse(bodyString);
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile('template/dashboard.html', 'utf8', (err, data) => {
       if (err) { throw err }
-      var values ={
-          username: post['username'],
-          loggedUsers:listLoginUsers(loggedInUsers)
+      var values = {
+        username: post['username'],
+      }
+        ;
+
+      var compiled = template(data, values)
+      response.write(compiled);
+      response.end();
+    })
+  })
+};
+
+var handleLoginPost = (request, response) => {
+
+  response.writeHead(200, { "Content-Type": "text/html" });
+  var bodyString = '';
+  request.on('data', function (data) {
+    bodyString += data;
+
+  });
+
+  request.on('end', function () {
+    var post = queryString.parse(bodyString);
+    loggedInUsers.push(post)
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile('template/dashboard.html', 'utf8', (err, data) => {
+      if (err) { throw err }
+      var values = {
+        username: post['username'],
+        loggedUsers: listLoginUsers(loggedInUsers)
       }
       var compiled = template(data, values)
       response.write(compiled);
       response.end();
     });
-  
+
   });
 }
-    // response.writeHead(200, {"Content-Type": "text/html"});
-    // fs.readFile('template/404.html', 'utf8', (err, data) => {
-    //     if (err) { throw err }
-    //     var values ={
-    //         username: post['username'],
-    //   }
-    //     var compiled = template(data, values)
-    //     response.write(compiled);
-    //     response.end();
-    //   })
-    // }
-    
+
+var handleNavigationPost = (request, response) => {
+
+  response.writeHead(200, { "Content-Type": "text/html" });
+  var bodyString = '';
+  request.on('data', function (data) {
+    bodyString += data;
+
+  });
+
+  request.on('end', function () {
+    var post = queryString.parse(bodyString);
+    loggedInUsers.push(post)
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile('template/chat2.html', 'utf8', (err, data) => {
+      if (err) { throw err }
+      var values = {
+        username: post['username'],
+        loggedUsers: listLoginUsers(loggedInUsers)
+      }
+      var compiled = template(data, values)
+      response.write(compiled);
+      response.end();
+    });
+
+  });
+}
 
 const listLoginUsers = (loggedUsers) => {
   var list = ''
-  for (user of loggedInUsers){
+  for (user of loggedInUsers) {
     list += `<p style="padding-left: 7%;">${user.username}<p>`
-      }
-      return list
+  }
+  return list
 }
 
-   
-   server.on("request", function(request, response) {
-     var handler = simpleRouter(request)
 
-     if (handler != null) {
-         handler(request, response)
-     } else {
-         response.writeHead(404);
-         response.end();
-     }
+server.on("request", function (request, response) {
+  var handler = simpleRouter(request)
+
+  if (handler != null) {
+    handler(request, response)
+  } else {
+    response.writeHead(404);
+    response.end();
+  }
 });
-    
-server.listen(8080, function (){
-    console.log('listening on port 8080...')
+
+server.listen(8080, function () {
+  console.log('listening on port 8080...')
 })
